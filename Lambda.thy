4d6da31c53558a0a20b37e50f6cf90ff9d4f3195
EOWNERDEAD <eownerdead@disroot.org>
2023-08-27 22:38:11 +0900

    Unfinished Church-Rosser property proof

--- Lambda.thy
+++ Lambda.thy
@@ -24,8 +24,85 @@ inductive beta :: "db ⇒ db ⇒ bool" (infix "→⇩β" 50) where
 | abs: "m →⇩β m' ⟹ (λm) →⇩β λm'"
 | beta: "(λm) ° n →⇩β m[0::=n]"
 
+abbreviation beta_reds :: "db ⇒ db ⇒ bool" (infix "→⇩β⇧*" 50) where
+  "x →⇩β⇧* y ≡ beta⇧*⇧* x y"
+
+(*inductive beta_star :: "db ⇒ db ⇒ bool" (infix "→⇩β⇧*" 50) where
+  refl: "x →⇩β⇧* x"
+| step: "x →⇩β y ⟹ y →⇩β⇧* z ⟹ x →⇩β⇧* z"*)
+
+(*lemma star_beta_abs:
+  assumes "m →⇩β⇧* m'"
+    shows "(λm) →⇩β⇧* λm'"
+  using assms
+proof (induct rule: rtranclp_induct)
+qed (auto intro: abs)*)
+
+lemma abs_eq:
+  assumes "(λm) →⇩β⇧* n" and "m →⇩β⇧* m'"
+    shows "n = λm'"
+  using assms
+  nitpick
+proof (induct m rule: db.induct)
+  case (Var x)
+  then show ?case sorry
+next
+  case (App x1a x2)
+  assume "(λm) →⇩β (x1a ° x2)"
+  hence "(λm) →⇩β (x1a ° x2) ⟹ False"
+  apply induct
+  apply auto
+  thus "False"
+next
+  case (Abs x)
+  then show ?case sorry
+qed (auto simp add: beta.beta beta.abs beta.appL beta.appR)  
+  
+  
+proof (induct n)
+  case abs
+  from assms show ?case by auto
+
+
+theorem diamond:
+  assumes "x →⇩β y" "x →⇩β z"
+    shows "∃u. y →⇩β⇧* u ∧ z →⇩β⇧* u"
+  using assms
+proof (induct x y arbitrary: z)
+  from assms show ?case by auto
+
+
+theorem diamond:
+  assumes "x →⇩β y" "x →⇩β z"
+    shows "∃u. y →⇩β⇧* u ∧ z →⇩β⇧* u"
+  using assms
+proof (induct x y arbitrary: z)
+  case (appL m m' n)
+  then show ?case by 
+next
+  case (appR n n' m)
+  then show ?case sorry
+next
+  case (abs m m')
+  assume 1:"m →⇩β m'"
+     and 2: "⋀z'. m →⇩β z' ⟹ ∃u. m' →⇩β⇧* u ∧ z' →⇩β⇧* u"
+     and 3: "(λm) →⇩β z"
+  from 3 and 1 have 4: "z = λm'" by (simp add: abs_eq)
+  from 1 have "(λm) →⇩β λm'" by (rule beta.abs)
+  have "∃u. (λm') →⇩β⇧* u" by auto
+  hence "∃u. (λm') →⇩β⇧* u ∧ (λm') →⇩β⇧* u" by auto
+  from 4 and this show ?case by auto
+next
+  case (beta m n)
+  then show ?case sorry
+qed
+
+     
+
 code_pred (modes: i ⇒ o ⇒ bool as reduce') beta .
 
 definition "reduce t = Predicate.the (reduce' t)"
+
+value "reduce ((λVar 0) ° Var 0) ° Var 0"
 
 end
